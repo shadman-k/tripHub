0a722d3326c52974647fbb1287fade20debe49e2
Shadman Khan <shadmankhan.825@gmail.com>
2021-04-20 19:10:29 -0400

    pushing to pull from main

--- client/App.jsx
+++ client/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+// import { Link } from 'react-router-dom'
 
 export default class App extends Component {
   constructor(props) {
@@ -9,6 +10,13 @@ export default class App extends Component {
     return (
       <div>
         <p>tripHub</p>
+        <div className="btncontainer btncontainer-fb">
+          {/* <Link to='/auth/facebook' className='btn-facebook'> */}
+          <a href="/auth/facebook" className="btn-fb">
+            Sign in with Facebook
+          </a>
+          {/* </Link> */}
+        </div>
       </div>
     )
   }
