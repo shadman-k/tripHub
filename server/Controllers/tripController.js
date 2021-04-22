f1f2a436c97e0fbba41a8a3db77efbee76a2f63e
Ziyad E. El Baz <ziyad.elbaz@me.com>
2021-04-21 22:00:53 -0500

    Updated controllers to integrate with frontent

--- server/Controllers/tripController.js
+++ server/Controllers/tripController.js
@@ -4,15 +4,30 @@ const uuidv4 =require('uuid').v4;
 const tripController = {};
 
 tripController.createTrips = (req, res, next) => {
-  const {name, destination, groupID, createdBy, dateStart, dateEnd} = req.body
+  const { name, destination, groupID, createdBy, dateStart, dateEnd } = req.body
+  
+  const newUUID = uuidv4();
 
   const query = `
-  INSERT INTO trip(trip_name, destination, groupID, createdBy, dateStart, dateEnd, trip_ID, members)
-  VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+  INSERT INTO trip(trip_name, destination, groupID, createdBy, dateStart, dateEnd, trip_ID)
+  VALUES ($1, $2, $3, $4, $5, $6, $7)
   `
 
-  db.query(query, [name, destination, groupID, createdBy, dateStart, dateEnd, uuidv4(), `${createdBy}`])
-    .then((data) => res.json(data)).catch((e) => { console.log(e) })
+  db.query(query, [name, destination, groupID, createdBy, dateStart, dateEnd, newUUID])
+    .then(() => {
+      console.log("new db")
+      const query = `
+      SELECT *
+      FROM trip
+      WHERE trip_id = '${newUUID}'` 
+
+      db.query(query).then((data) => {
+        res.json(data)
+      }).catch((e) => { console.log(e) })
+    }
+    
+  )
+    .catch((e) => { console.log(e)})
 }
 
 tripController.addMember = (req, res, next) => {
@@ -25,15 +40,17 @@ tripController.addMember = (req, res, next) => {
   `
 
   db.query(getQuery).then((data) => {
-    const members = data.rows[0].members.length
+    const response = data.rows[0].members
+    const members = response ? response.length : 0;
 
     const query = `
     UPDATE trip
     SET members[${members+1}] = '${memberUUID}'
     WHERE trip_ID = '${trip_ID}'
-  `
+    `
+
     db.query(query)
-      .then((data) => { res.json(data) })
+      .then((data) => { res.json(memberUUID) })
       .catch((e) => { console.log(e) })
 
   }).catch((e) => { console.log(e) })
@@ -72,20 +89,57 @@ tripController.getTripsAll = (req, res, next) => {
    SELECT *
    FROM trip
   `
-  db.query(query).then((data) => {res.json(data)}).catch((e) => {console.log(e)})
+  db.query(query).then((data) => {
+    res.locals.rows = data.rows
+    next()
+  }).catch((e) => { console.log(e) })
 }
 
-tripController.getTripsOne = (req, res, next) => {
-  const { trip_ID } = req.body
+tripController.tripsParser = (req, res, next) => {
+  console.log(res.locals.uuid)
+  const package = [];
+  const selectedItems = res.locals.rows
   
+  for (let i = 0; i < selectedItems.length; i++) {
+    const element = selectedItems[i];
+    const members = element.members ? element.members[0].split(',') : '';
+   
+    if (element.createdby === res.locals.uuid) {
+      package.push(element)
+    } else if (members.includes(res.locals.uuid)) {
+      package.push(element)
+    }
+    
+
+  }
+
+  res.package = package;
+  next()
+}
+
+tripController.getTripsCreatedBy = (req, res, next) => {  
   const query = `
    SELECT *
    FROM trip
-   WHERE trip_ID = '${trip_ID}'
+   WHERE createdBy = '${res.locals.uuid}'
   `
   db.query(query).then((data) => {
-    res.json(data)
+    res.locals.createdBy = data
+    next();
   }).catch((e) => { console.log(e) })
 }
+
+tripController.getTripsMemberOf = (req, res, next) => {  
+  const query = `
+   SELECT *
+   FROM trip
+   WHERE createdBy = '${res.locals.uuid}'
+  `
+  db.query(query).then((data) => {
+    res.locals.createdBy = data
+    next();
+  }).catch((e) => { console.log(e) })
+}
+
 
 module.exports = tripController
